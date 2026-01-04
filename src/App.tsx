7a54fe188f04dada322d58f60615c982b452015d
navspex <navspex@gmail.com>
2026-01-04 02:37:58 -0500

    Fix: ebay-feedback extension .png -> .jpg

--- src/App.tsx
+++ src/App.tsx
@@ -148,7 +148,7 @@ function App() {
                 className="bg-white rounded-xl shadow-lg p-6 hover:shadow-xl transition-shadow border border-gray-100"
               >
                 <img 
-                  src="/images/ebay-feedback.png" 
+                  src="/images/ebay-feedback.jpg" 
                   alt="ESSCO Aircraft eBay Feedback - 100% Positive Rating" 
                   className="h-20 w-auto mx-auto"
                   onError={(e) => { 
